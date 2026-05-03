f05b8282eb254822056fae71da2081ca5b0d7688
Salino3 <flaviobcn012@gmail.com>
2026-05-03 15:29:55 +0200

    add styles layout

--- src/layout/home/home.layout.tsx
+++ src/layout/home/home.layout.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import clsx from "clsx";
 import { BoxText } from "../../common-app";
 
 export const LayoutHome: React.FC = () => {
+  const containerStyles = clsx(`
+    flex flex-col items-center justify-center min-h-[100dvh] relative 
+   [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden
+    `);
+
   return (
-    <main className=" bg-black">
+    <main className={` bg-black ${containerStyles}`}>
       <div>
         <BoxText title="Name" value="Joe" />
       </div>
