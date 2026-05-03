d4430ac927243ff527030ce874c5ce7e15652f48
Salino3 <flaviobcn012@gmail.com>
2026-05-03 16:15:36 +0200

    save changes

--- src/common-app/main-header/main-header.component.tsx
+++ src/common-app/main-header/main-header.component.tsx
@@ -1,20 +1,16 @@
 import clsx from "clsx";
+import { containerStyles } from "./styles";
 
 export const MainHeader: React.FC = () => {
-  const containerStyles = clsx(`
-        flex flex-row items-center justify-center  
-        w-full bg-rfe-primary-container h-[85px]
-        `);
-
   const navStyles = clsx(`
         flex flex-row items-center justify-center  
-        w-full bg-rfe-primary-container h-[85px]
+        border-red-700 border-2 w-full
         `);
 
   return (
     <header data-component="MainHeader" className={containerStyles}>
-      <div>
-        <h3 className="flex items-center justify-center w-[160px] h-[32px] text-rfe-title-pink font-bold font-[liberation-serif] text-[22px]">
+      <div className="flex flex-row justify-between items-center gap-2 w-[500px] flex-none  border-slate-600 border-2">
+        <h3 className="flex items-center justify-center min-w-[160px] h-[32px] text-rfe-title-pink font-bold font-[liberation-serif] text-[22px]">
           Maison du Chat
         </h3>
         <nav className={navStyles}>
---
+++ src/common-app/main-header/styles.ts
@@ -0,0 +1,7 @@
+import clsx from "clsx";
+
+export const containerStyles = clsx(`
+        flex flex-row items-center justify-center  
+        w-full bg-rfe-primary-container h-[85px]
+        
+        `);
