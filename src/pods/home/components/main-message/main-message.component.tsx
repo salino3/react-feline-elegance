d8526ba64392931cc9adc791b0f63f7c82740aae
Salino3 <flaviobcn012@gmail.com>
2026-05-04 16:08:41 +0200

    fix

--- src/pods/home/components/main-message/main-message.component.tsx
+++ src/pods/home/components/main-message/main-message.component.tsx
@@ -4,17 +4,19 @@ export const MainMessage: React.FC = () => {
   return (
     <div
       data-component="MainMessage"
-      className="absolute -top-[-50%] -left-[-50%]   flex flex-col justify-center items-center    max-w-[1152px]  z-[2] "
+      className="absolute px-5   flex-1 w-full h-full flex flex-col   justify-center items-center  z-[2] "
     >
-      <h3 className="font-[Noto-Serif] text-[16px] text-white">
-        L'Art de Vivre Feline
-      </h3>
-      <p className="font-[Italic] text-[16px] text-white text-center">
-        Curated luxury essentials for the modern companion
-      </p>
-      <button className="cursor-pointer  bg-[#88a17b]  border border-rfe-on-secondary-fixed-variant text-rfe-on-secondary-fixed-variant px-8 py-4 rounded-full font-semibold hover:bg-rfe-secondary-container   transition-colors">
-        Discover the Collection
-      </button>
+      <div className="  flex-1 w-full h-full flex flex-col gap-1  justify-center items-center">
+        <h3 className="font-[Noto-Serif] text-[16px] text-white">
+          L'Art de Vivre Feline
+        </h3>
+        <p className="font-[Italic] text-[16px] text-white text-center">
+          Curated luxury essentials for the modern companion
+        </p>
+        <button className="cursor-pointer  bg-[#88a17b]  border border-rfe-on-secondary-fixed-variant text-rfe-on-secondary-fixed-variant px-8 py-4 rounded-full font-semibold hover:bg-rfe-secondary-container   transition-colors">
+          Discover the Collection
+        </button>
+      </div>
     </div>
   );
 };
